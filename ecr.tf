2a0711fdd3730c7d6cd09a9e7cfe8903bc2fd336
lily4499 <77320224+lily4499@users.noreply.github.com>
2024-02-22 13:15:07 -0500

    Update ecr.tf

--- ecr.tf
+++ ecr.tf
@@ -1,9 +1,46 @@
-# Create ECR repository
-resource "aws_ecr_repository" "my_repository" {
-  name = "my-ecr-repository"
+# Create an IAM user
+resource "aws_iam_user" "ecr_user" {
+  name = "ecr_user"
+}
+
+
+# Create a policy allowing the IAM user to manage ECR repositories
+resource "aws_iam_policy" "ecr_policy" {
+  name        = "ecr_policy"
+  description = "Policy for managing ECR repositories"
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect   = "Allow",
+      Action   = [
+        "ecr:GetAuthorizationToken",
+        "ecr:BatchCheckLayerAvailability",
+        "ecr:GetDownloadUrlForLayer",
+        "ecr:BatchGetImage",
+        "ecr:InitiateLayerUpload",
+        "ecr:UploadLayerPart",
+        "ecr:CompleteLayerUpload",
+        "ecr:PutImage"
+      ],
+      Resource = "*"
+    }]
+  })
+}
+
+# Attach the policy to the IAM user
+resource "aws_iam_user_policy_attachment" "ecr_policy_attachment" {
+  user       = aws_iam_user.ecr_user.name
+  policy_arn = aws_iam_policy.ecr_policy.arn
+}
+
+# Create the ECR repository
+resource "aws_ecr_repository" "lil_repository" {
+  name = "amazon-ecr"
+  # Additional repository configurations can be added here if needed
 }
 
 # Output ECR repository URL
 output "ecr_repository_url" {
-  value = "${aws_ecr_repository.my_repository.repository_url}"
+  value = "${aws_ecr_repository.lil_repository.repository_url}"
 }
