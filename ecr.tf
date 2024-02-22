4bde3b20fdffa2cd846d2d6625d5c58298fa278b
lily4499 <77320224+lily4499@users.noreply.github.com>
2024-02-21 22:45:09 -0500

    Update ecr.tf

--- ecr.tf
+++ ecr.tf
@@ -2,3 +2,8 @@
 resource "aws_ecr_repository" "my_repository" {
   name = "my-ecr-repository"
 }
+
+# Output ECR repository URL
+output "ecr_repository_url" {
+  value = "${aws_ecr_repository.my_repository.repository_url}"
+}
